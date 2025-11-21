fea23d7a27a2aab10a2e16ec9833b95499b46c65
diegomez13 <diegomez13@hotmail.com>
2025-11-21 18:38:40 +0100

    a

--- Diego/Concurrencia/main.go
+++ Diego/Concurrencia/main.go
@@ -9,16 +9,17 @@ import (
 type Respuesta struct {
 	Status bool
 	Valor  int
+	Indice int
 }
 
 func main() {
 
 	var wg sync.WaitGroup
-	ch := make(chan Respuesta)
+	ch := make(chan []Respuesta)
 
 	for i := 0; i < 10; i++ {
 		wg.Add(1)
-		go Test(&wg, ch, i)
+		go Test2(&wg, ch, i)
 	}
 
 	go func() {
@@ -31,11 +32,20 @@ func main() {
 	}
 
 }
-func Silence(r Respuesta) {
 
-}
 func Test(wg *sync.WaitGroup, ch chan<- Respuesta, i int) {
 	defer wg.Done()
 	time.Sleep(time.Duration(i*3) * time.Second)
 	ch <- Respuesta{Status: true, Valor: i}
+}
+
+func Test2(wg *sync.WaitGroup, ch chan<- []Respuesta, i int) {
+	defer wg.Done()
+	var Total int
+	for j := 0; j < (9-i)*1000000000; j++ {
+		Total = Total + j
+	}
+
+	ch <- []Respuesta{Respuesta{Status: true, Valor: Total, Indice: i}, Respuesta{Status: true, Valor: Total, Indice: i}}
+
 }
