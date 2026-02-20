cd12604b8cc327aa936114866f840cbf3672fc86
diegomez13 <diegomez13@hotmail.com>
2026-02-20 15:14:30 -0300

    a

--- modules/bases/bases.go
+++ modules/bases/bases.go
@@ -1,7 +1,28 @@
 package bases
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func Test() {
 	fmt.Println("Prube exitosa")
+}
+func GetOps(t string, lt []string, tipo int) string {
+
+	if tipo == 0 {
+		fmt.Println(t)
+	}
+	if tipo == 1 {
+		fmt.Print(t)
+	}
+
+	for _, x := range lt {
+		fmt.Println(x)
+	}
+	reader := bufio.NewReader(os.Stdin)
+	op, _ := reader.ReadString('\n')
+	return strings.TrimSpace(op)
 }
